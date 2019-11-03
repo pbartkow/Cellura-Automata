72cf162d4c5de15c66a07732860b3c6ed00b543a
Przemek Bartkowski <pw.bartkowski@gmail.com>
2019-11-03 17:35:20 +0100

    Implementation of basic cellura automata including rules

---
+++ Cellura automata/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Rules rules = new Rules();
+            Console.WriteLine("Podaj liczbe iteracji");
+            string amounOfIteration = Console.ReadLine();
+            int numberOfIteration = int.Parse(amounOfIteration);
+            int numberOfCells = 50;
+         
+            int[,] tab = new int[numberOfIteration,numberOfCells+2];
+
+            for (int i = 0; i < numberOfIteration; i++)
+            {
+                for (int j = 0; j < numberOfCells+2; j++)
+                {
+                    if (i == 0 && j == (numberOfCells+2)/2)
+                        tab[i, j] = 1;
+                    else 
+                        tab[i, j] = 0;
+                }
+            }
+
+            rules.Rule30(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine("Rule 30 - Results: ");
+            rules.showResults(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine();
+            rules.Rule60(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine("Rule 60 - Results: ");
+            rules.showResults(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine();
+            rules.Rule90(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine("Rule 90 - Results: ");
+            rules.showResults(tab, numberOfIteration, numberOfCells);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+       
+    }
+
+   
+}
---
+++ Cellura automata/Rules.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Rules
+    {
+        public void Rule30(int[,] tab, int numberOfIteration, int numberOfCells)
+        {
+            for (int i = 1; i < numberOfIteration; i++)
+            {
+                for (int j = 1; j < numberOfCells + 1; j++)
+                {
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+
+                }
+            }
+        }
+        public void Rule90(int[,] tab, int numberOfIteration, int numberOfCells)
+        {
+            for (int i = 1; i < numberOfIteration; i++)
+            {
+                for (int j = 1; j < numberOfCells + 1; j++)
+                {
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+
+                }
+            }
+        }
+        public void Rule60(int[,] tab, int numberOfIteration, int numberOfCells)
+        {
+            for (int i = 1; i < numberOfIteration; i++)
+            {
+                for (int j = 1; j < numberOfCells + 1; j++)
+                {
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 1;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1)
+                        tab[i, j] = 0;
+                    if (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0)
+                        tab[i, j] = 0;
+
+                }
+            }
+        }
+        public void showResults(int[,] tab, int iterations, int cells)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                for (int j = 0; j < cells + 2; j++)
+                {
+                    Console.Write(" " + tab[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
